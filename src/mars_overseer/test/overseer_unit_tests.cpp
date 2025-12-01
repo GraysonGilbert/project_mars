16eae8abb9e2318c4a0897b1d93433796a3c485d
Grayson Gilbert <graysongilbert@gmail.com>
2025-12-01 23:29:34 +0000

    Added doxygen comments and improved inline documentation

--- src/mars_overseer/test/overseer_unit_tests.cpp
+++ src/mars_overseer/test/overseer_unit_tests.cpp
@@ -1,22 +1,31 @@
 /**
  * @file overseer_unit_tests.hpp
- * @author your name (you@domain.com)
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2025-11-30
- * 
- * @copyright Copyright (c) 2025
- * 
+ * @author Grayson Gilbert (ggilbert@umd.edu)
+ * @author Marcus Hurt (mhurt@umd.edu)
+ * @brief Unittest cases for map_merging functionality
+ * @copyright MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include <gtest/gtest.h>
 #include "mars_overseer/map_merger.hpp"
-
-
-
-// Simple test to ensure that the test framework is working
-TEST(SanityCheck, BuildPasses) { EXPECT_EQ(1, 1); }
 
 
 // ===================== TEST CASES: =====================
