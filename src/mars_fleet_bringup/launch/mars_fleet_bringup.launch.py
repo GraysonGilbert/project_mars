58cfb7493d6601e740aee8647b111adf5bcd74e6
Grayson Gilbert <graysongilbert@gmail.com>
2025-12-06 20:20:57 +0000

    Modified bringup to add launch argument specifying number of robots in simulation

--- src/mars_fleet_bringup/launch/mars_fleet_bringup.launch.py
+++ src/mars_fleet_bringup/launch/mars_fleet_bringup.launch.py
@@ -1,7 +1,7 @@
 import os
 
 import launch
-from launch import LaunchDescription
+from launch import LaunchDescription, LaunchContext
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler, EmitEvent
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
@@ -10,6 +10,27 @@ from launch.event_handlers import OnProcessExit
 
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
+from launch.actions import OpaqueFunction
+
+
+def launch_robots(context: LaunchContext, *args, **kwargs):
+    robot_launch_file = kwargs['robot_launch_file']
+    num_robots = int(LaunchConfiguration('num_robots').perform(context))
+    use_sim_time = LaunchConfiguration('use_sim_time').perform(context)
+
+    actions = []
+    for i in range(num_robots):
+        robot_id = f'robot_{i+1}'
+        actions.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(robot_launch_file),
+                launch_arguments={
+                    'robot_id': robot_id,
+                    'use_sim_time': use_sim_time,
+                }.items()
+            )
+        )
+    return actions
 
 
 def generate_launch_description():
@@ -22,9 +43,12 @@ def generate_launch_description():
     )
 
     package_dir = get_package_share_directory(fleet_package)
-    world_path = os.path.join(package_dir, 'worlds', 'mars_two_robots.wbt')
+    world_path = os.path.join(package_dir, 'worlds', 'mars_2_robots.wbt')
 
     # Launch arguments
+    num_robots_arg = DeclareLaunchArgument(
+        'num_robots', default_value='2', description='Number of robots to launch'
+    )
     use_sim_time_arg = DeclareLaunchArgument(
         'use_sim_time', default_value='true', description='Use simulation (Webots) clock'
     )
@@ -32,25 +56,11 @@ def generate_launch_description():
         'record_rosbag', default_value='false', description='Enable rosbag recording (true/false)'
     )
 
-    use_sim_time = LaunchConfiguration('use_sim_time')
     record_rosbag = LaunchConfiguration('record_rosbag')
 
     # Webots simulator
     webots = WebotsLauncher(world=world_path, ros2_supervisor=True)
-
-    # Multiple robot instances
-    robots = []
-    for robot_id in ['robot_1', 'robot_2']:
-        robots.append(
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(robot_launch_file),
-                launch_arguments={
-                    'robot_id': robot_id,
-                    'use_sim_time': use_sim_time,
-                }.items(),
-            )
-        )
-
+   
     # Optional global rosbag recording
     rosbag_recorder = ExecuteProcess(
         cmd=['ros2', 'bag', 'record', '-a', '-x', '/camera/.*'],
@@ -64,14 +74,14 @@ def generate_launch_description():
             target_action=webots,
             on_exit=[EmitEvent(event=launch.events.Shutdown())],
         )
-    )
+    )    
+       
+    ld = LaunchDescription([num_robots_arg, use_sim_time_arg, record_rosbag_arg])   
+    
+    ld.add_action(webots)
+    ld.add_action(webots._supervisor)
+    ld.add_action(OpaqueFunction(function=launch_robots, kwargs={'robot_launch_file': robot_launch_file}))
+    ld.add_action(rosbag_recorder)
+    ld.add_action(shutdown_on_webots_exit)
 
-    return LaunchDescription([
-        use_sim_time_arg,
-        record_rosbag_arg,
-        webots,
-        webots._supervisor,
-        *robots,
-        rosbag_recorder,
-        shutdown_on_webots_exit,
-    ])
+    return ld
